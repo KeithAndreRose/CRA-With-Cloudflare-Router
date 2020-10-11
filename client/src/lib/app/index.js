3951c4df808e6820394cdbd67eae191160836a66
Keith Rose <andrerose722@gmail.com>
2020-10-11 08:14:48 -0400

    removed unwanted additional context

--- client/src/lib/app/index.js
+++ client/src/lib/app/index.js
@@ -1,6 +1,1 @@
-import { createContext } from "react"
-
-export const DispatchContext = createContext()
-export const StateContext = createContext()
-
 export {default as AppReducer} from './reducers/AppReducer'
