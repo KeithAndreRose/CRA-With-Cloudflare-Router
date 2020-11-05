60c34f8a1e41229a9863c1278a3c2d5359a29efb
Keith Rose <andrerose722@gmail.com>
2020-11-05 18:32:49 -0500

    refactoring

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,10 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
-import { useFetch } from './hooks';
 import EventReducer from './lib/events';
 import { DispatchProvider, StateProvider } from './lib/providers';
 import { IndexRoute, NotFoundRoute } from './routes';
 import './scss/App.scss';
-
-/**
- * The client app is served via a Cloudflare worker
- * The worker can also also compute requests similar to "/functions" from Netlify
- */
-const apiEndpoint = 
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:8787/api' // Local `wrangler dev` instance
-    : '/api' // Live Instance
 
 function App() {
   const [state, dispatch] = useReducer(EventReducer, {})
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,15 @@ import './scss/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/**
+ * The client app is served via a Cloudflare worker
+ * The handlers can be called at endpoints using "/api"
+ */
+export const apiEndpoint = 
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8787/api' // Local `wrangler dev` instance
+    : '/api'; // Live Instance
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
