05201fbc7ba3638ac7f897008b832c3ce7a2e698
Keith Rose <andrerose722@gmail.com>
2020-11-05 18:40:13 -0500

    refactoring

---
+++ client/src/routes/views/IndexView.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './scss/IndexView.scss'
+
+export default function IndexView() {
+  return (
+    <div className="IndexView">
+      <h1 className="IndexView-heading">Atomic Cotton</h1>
+      <div className="IndexView-body">
+        Loading...
+      </div>
+    </div>
+  )
+}
